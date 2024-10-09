53c44b59c3f9c2b9e57346c47a79a9910587e965
EgPwl <egpwl07@gmail.com>
2024-10-09 23:28:39 +0900

    초기 설정

---
+++ src/main/java/com/example/localens/LocalensApplication.java
@@ -0,0 +1,13 @@
+package com.example.localens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/localens/LocalensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
