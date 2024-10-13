788e3986a8c4cd551c03a772fc0df1e99dbffa79
EgPwl <egpwl07@gmail.com>
2024-10-14 00:32:41 +0900

    feat : MemberResponseDto 추가

--- src/main/java/com/example/localens/member/dto/MemberResponseDto.java
+++ src/main/java/com/example/localens/member/dto/MemberResponseDto.java
@@ -1,2 +1,17 @@
-package com.example.localens.member.dto;public class MemberResponseDto {
+package com.example.localens.member.dto;
+
+import com.example.localens.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberResponseDto {
+    private String email;
+
+    public static MemberResponseDto of(Member member) {
+        return new MemberResponseDto(member.getEmail());
+    }
 }
