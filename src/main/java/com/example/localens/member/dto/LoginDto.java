f8b901ddaa69849afb949e1fc6452fc9a285d6ff
EgPwl <egpwl07@gmail.com>
2024-10-14 00:24:46 +0900

    feat : LoginDto, TokenDto, TokenRequestDto 추가

---
+++ src/main/java/com/example/localens/member/dto/LoginDto.java
@@ -0,0 +1,18 @@
+package com.example.localens.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    private String email;
+    private String password;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
+}
