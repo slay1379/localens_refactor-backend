35df597f47ab01426afed6f396d994eaac0c55ed
EgPwl <egpwl07@gmail.com>
2024-10-14 11:20:31 +0900

    A[ã[feat]: AuthService, CustomUserDetailsService, MemberService 추가

---
+++ src/main/java/com/example/localens/member/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.example.localens.member.service;
+
+import com.example.localens.member.domain.Member;
+import com.example.localens.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+
+        return new User(
+                String.valueOf(member.getMemberUuid()),
+                member.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
