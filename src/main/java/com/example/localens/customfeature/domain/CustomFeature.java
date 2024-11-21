a8b5de547acf4121a791a1375459de1aaebd71da
slay <palgalow@gmail.com>
2024-11-21 22:57:56 +0900

    [feat]: CustomFeature 객체 클래스 생성

---
+++ src/main/java/com/example/localens/customfeature/domain/CustomFeature.java
@@ -0,0 +1,32 @@
+package com.example.localens.customfeature.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Entity
+@Table(name = "custom_features")
+@Getter
+public class CustomFeature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long featureId;
+
+    private Long userId;
+    private String featureName;
+
+    @Column(length = 1000)
+    private String formula;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public CustomFeature() {
+    }
+}
