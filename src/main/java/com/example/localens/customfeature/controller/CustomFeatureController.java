5e8ab71df366c37f923d9f71cec4f4cf866c731d
slay <palgalow@gmail.com>
2024-11-21 23:31:59 +0900

    [feat]: 수식 검증 메서드 기능 추가

--- src/main/java/com/example/localens/customfeature/controller/CustomFeatureController.java
+++ src/main/java/com/example/localens/customfeature/controller/CustomFeatureController.java
@@ -3,7 +3,11 @@ package com.example.localens.customfeature.controller;
 import com.example.localens.customfeature.domain.CustomFeature;
 import com.example.localens.customfeature.service.CustomFeatureService;
 import com.example.localens.influx.InfluxDBService;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -27,11 +31,33 @@ public class CustomFeatureController {
         this.influxDBService = influxDBService;
     }
 
-    //피처 생성 폼
+    // 피처 생성 폼
     @GetMapping("/new")
     public String showCustomFeatureForm(Model model) {
         model.addAttribute("custom_feature", new CustomFeature());
         model.addAttribute("dataColumns", influxDBService.getFieldKeys(measurement));
         return "custom_feature_form";
+    }
+
+    // 수식 검증 메서드
+    private boolean isValidFormula(String formula) {
+        try {
+            // 변수 목록을 InfluxDBService에서 가져옴
+            Set<String> variables = new HashSet<>(influxDBService.getFieldKeys(measurement));
+
+            // 수식 파싱
+            Expression e = new ExpressionBuilder(formula)
+                    .variables(variables)
+                    .build();
+
+            for (String var : variables) {
+                e.setVariable(var, 1);
+            }
+
+            e.evaluate();
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
     }
 }
