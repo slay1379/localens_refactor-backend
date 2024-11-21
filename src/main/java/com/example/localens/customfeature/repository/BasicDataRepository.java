a9affeb04f302752a42ba98d3707279a9dbed63f
slay <palgalow@gmail.com>
2024-11-21 22:28:40 +0900

    [feat]: influxDB 설정 클래스 추가

--- src/main/java/com/example/localens/customfeature/repository/BasicDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.localens.customfeature.repository;
-
-import com.example.localens.customfeature.domain.BasicData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BasicDataRepository extends JpaRepository<BasicData, Long> {
-}
