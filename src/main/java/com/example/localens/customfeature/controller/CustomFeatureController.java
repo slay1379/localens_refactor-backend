196b1d74d235a4cdc825c7eddfbc0ba025bfac4b
slay <palgalow@gmail.com>
2024-11-21 23:35:56 +0900

    [feat]: 커스텀 피처 생성 처리 기능 추가

--- src/main/java/com/example/localens/customfeature/controller/CustomFeatureController.java
+++ src/main/java/com/example/localens/customfeature/controller/CustomFeatureController.java
@@ -13,7 +13,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/customFeatures")
@@ -37,6 +40,23 @@ public class CustomFeatureController {
         model.addAttribute("custom_feature", new CustomFeature());
         model.addAttribute("dataColumns", influxDBService.getFieldKeys(measurement));
         return "custom_feature_form";
+    }
+
+    // 피처 생성 처리
+    @PostMapping
+    public String createCustomFeature(@ModelAttribute CustomFeature customFeature,
+                                      RedirectAttributes redirectAttributes) {
+        // Long userId = getCurrentUserId(); 현재 사용자ID 설정
+        // customFeature.setUserId(userId);
+
+        if (!isValidFormula(customFeature.getFormula())) {
+            redirectAttributes.addFlashAttribute("message", "유효하지 않은 수식입니다.");
+            return "redirect:/customFeatures/new";
+        }
+
+        customFeatureService.saveCustomFeature(customFeature);
+        redirectAttributes.addFlashAttribute("message", "피처가 성공적으로 생성되었습니다.");
+        return "redirect:/customFeatures";
     }
 
     // 수식 검증 메서드
