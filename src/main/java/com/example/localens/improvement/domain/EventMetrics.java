22a326d7a4e24d3d40e120c1214e24b98ff9bbb8
slay <palgalow@gmail.com>
2024-11-25 23:54:31 +0900

    [fix]: getter setter import 추가

--- src/main/java/com/example/localens/improvement/domain/EventMetrics.java
+++ src/main/java/com/example/localens/improvement/domain/EventMetrics.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "event_metrics")
