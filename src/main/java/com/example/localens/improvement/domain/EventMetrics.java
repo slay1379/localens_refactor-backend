8430f267de12148450abd5474a3ef4ca9f239c79
slay <palgalow@gmail.com>
2024-11-25 23:45:19 +0900

    [feat]: EventMetricsId 도메인 생성

---
+++ src/main/java/com/example/localens/improvement/domain/EventMetrics.java
@@ -0,0 +1,21 @@
+package com.example.localens.improvement.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "event_metrics")
+@IdClass(EventMetricsId.class)
+public class EventMetrics {
+
+    @Id
+    @Column(name = "event_uuid")
+    private String eventUuid;
+
+    @Id
+    @Column(name = "metrics_uuid")
+    private String metricsUuid;
+}
