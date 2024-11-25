d6fd8fd43b8c906f2d84a337711d6023a55ef280
slay <palgalow@gmail.com>
2024-11-25 23:42:09 +0900

    [refactor]: event 도메인 수정

--- src/main/java/com/example/localens/improvement/repository/ClusterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.localens.improvement.repository;
-
-import com.example.localens.improvement.domain.Cluster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClusterRepository extends JpaRepository<Cluster, String> {
-}
