a48bfc66ec32741bc9ba53efbce5d34c231f7ce5
slay <palgalow@gmail.com>
2024-11-25 23:47:04 +0900

    [feat]: EventMetrics 레포지토리 생성

---
+++ src/main/java/com/example/localens/improvement/repository/EventMetricsRepository.java
@@ -0,0 +1,11 @@
+package com.example.localens.improvement.repository;
+
+import com.example.localens.improvement.domain.Event;
+import com.example.localens.improvement.domain.EventMetrics;
+import com.example.localens.improvement.domain.EventMetricsId;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventMetricsRepository extends JpaRepository<EventMetrics, EventMetricsId> {
+    List<EventMetrics> findByMetricsUuidIn(List<String> metricsUuids);
+}
