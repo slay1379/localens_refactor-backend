e5e93efbb7948d20135de4b86cbf65134f160e6b
slay <palgalow@gmail.com>
2024-11-28 02:51:54 +0900

    [feat]: Id로 모든 이벤트 찾는 기능 추가

--- src/main/java/com/example/localens/improvement/repository/EventRepository.java
+++ src/main/java/com/example/localens/improvement/repository/EventRepository.java
@@ -1,7 +1,9 @@
 package com.example.localens.improvement.repository;
 
 import com.example.localens.improvement.domain.Event;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, String> {
+    List<Event> findAllById(Iterable<String> eventUuids);
 }
