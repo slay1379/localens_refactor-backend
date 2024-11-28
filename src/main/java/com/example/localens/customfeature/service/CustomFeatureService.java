a024366ac0a81b506a00641ee2e4686d791318a5
slay <palgalow@gmail.com>
2024-11-28 22:10:19 +0900

    [refactor]: 커스텀피쳐 멤버 uuid로 찾도록 수정

--- src/main/java/com/example/localens/customfeature/service/CustomFeatureService.java
+++ src/main/java/com/example/localens/customfeature/service/CustomFeatureService.java
@@ -2,6 +2,8 @@ package com.example.localens.customfeature.service;
 
 import com.example.localens.customfeature.domain.CustomFeature;
 import com.example.localens.customfeature.repository.CustomFeatureRepository;
+import com.example.localens.member.domain.Member;
+import com.example.localens.member.repository.MemberRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,18 +12,25 @@ import org.springframework.stereotype.Service;
 public class CustomFeatureService {
 
     private final CustomFeatureRepository customFeatureRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public CustomFeatureService(CustomFeatureRepository customFeatureRepository) {
+    public CustomFeatureService(CustomFeatureRepository customFeatureRepository,
+                                MemberRepository memberRepository) {
         this.customFeatureRepository = customFeatureRepository;
+        this.memberRepository = memberRepository;
     }
 
     public CustomFeature saveCustomFeature(CustomFeature customFeature) {
         return customFeatureRepository.save(customFeature);
     }
 
-    public List<CustomFeature> getCustomFeaturesByUserId(Long userId) {
-        return customFeatureRepository.findByUserId(userId);
+    public List<CustomFeature> getCustomFeatureByUserUuid(String userUuid) {
+        Member member = memberRepository.findById(userUuid).orElse(null);
+        if (member == null) {
+            return List.of();
+        }
+        return customFeatureRepository.findByMember(member);
     }
 
     public CustomFeature getCustomFeatureById(Long customFeatureId) {
