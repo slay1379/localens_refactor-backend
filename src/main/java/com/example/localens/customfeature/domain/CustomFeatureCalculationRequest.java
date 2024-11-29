257775ad2f7fe8a25cb426e6709352a9f4d89db3
slay <palgalow@gmail.com>
2024-11-29 23:38:11 +0900

    [feat]: 수식 계산하는 객체 생성

---
+++ src/main/java/com/example/localens/customfeature/domain/CustomFeatureCalculationRequest.java
@@ -0,0 +1,12 @@
+package com.example.localens.customfeature.domain;
+
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomFeatureCalculationRequest {
+    private String formula;
+    private Map<String, Double> variables;
+}
