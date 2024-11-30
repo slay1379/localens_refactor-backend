034952b5caefaaee9264d3ad47034b87da67c688
EgPwl <egpwl07@gmail.com>
2024-12-01 00:25:00 +0900

    [refactor]: PopulationController 반환 형식 변경

--- src/main/java/com/example/localens/analysis/controller/PopulationController.java
+++ src/main/java/com/example/localens/analysis/controller/PopulationController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/population")
@@ -25,21 +27,37 @@ public class PopulationController {
     private final AgeGenderRatioService ageGenderRatioService;
     private final NationalityRatioService nationalityRatioService;
 
-    @GetMapping("/all-ratios/{districtUuid}")
-    public ResponseEntity<List<Object>> getAllRatiosByDistrictUuid(@PathVariable Integer districtUuid) {
-        List<Object> results = new ArrayList<>();
 
-        // 각 서비스 호출 결과를 리스트에 추가
-        results.add(populationRatioService.getPopulationRatioByDistrictUuid(districtUuid));
-        results.add(stayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid));
-        results.add(congestionRateService.getCongestionRateByDistrictUuid(districtUuid));
-        results.add(stayPerVisitorService.getStayPopulationRatioByDistrictUuid(districtUuid));
-        results.add(stayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid));
-        results.add(ageGenderRatioService.getAgeGenderPopulationRatio(districtUuid));
-        results.add(nationalityRatioService.getNationalityPopulationRatio(districtUuid));
+    @GetMapping("/all-ratios/{districtUuid}")
+    public ResponseEntity<Map<String, Object>> getAllRatiosByDistrictUuid(@PathVariable Integer districtUuid) {
+
+        Map<String, Object> results = new LinkedHashMap<>();
+        results.put("시간대별_유동인구수", populationRatioService.getPopulationRatioByDistrictUuid(districtUuid).get시간대별_유동인구수());
+        results.put("시간대별_체류방문비율", stayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid).get시간대별_체류방문비율());
+        results.put("시간대별_혼잡도_변화율", congestionRateService.getCongestionRateByDistrictUuid(districtUuid).get시간대별_혼잡도_변화율());
+        results.put("체류시간_대비_방문자수", stayPerVisitorService.getStayPopulationRatioByDistrictUuid(districtUuid).get체류시간_대비_방문자수());
+        results.put("시간대별_평균_체류시간_변화율", stayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid).get시간대별_평균_체류시간_변화율());
+        results.put("연령대별_체류패턴", ageGenderRatioService.getAgeGenderPopulationRatio(districtUuid).get연령대별_체류패턴());
+        results.put("국적별_체류패턴", nationalityRatioService.getNationalityPopulationRatio(districtUuid).get국적별_체류패턴());
 
         return ResponseEntity.ok(results);
     }
+
+//    @GetMapping("/all-ratios/{districtUuid}")
+//    public ResponseEntity<List<Object>> getAllRatiosByDistrictUuid(@PathVariable Integer districtUuid) {
+//        List<Object> results = new ArrayList<>();
+//
+//        // 각 서비스 호출 결과를 리스트에 추가
+//        results.add(populationRatioService.getPopulationRatioByDistrictUuid(districtUuid));
+//        results.add(stayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid));
+//        results.add(congestionRateService.getCongestionRateByDistrictUuid(districtUuid));
+//        results.add(stayPerVisitorService.getStayPopulationRatioByDistrictUuid(districtUuid));
+//        results.add(stayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid));
+//        results.add(ageGenderRatioService.getAgeGenderPopulationRatio(districtUuid));
+//        results.add(nationalityRatioService.getNationalityPopulationRatio(districtUuid));
+//
+//        return ResponseEntity.ok(results);
+//    }
 
 //    @GetMapping("/population-ratio/{districtUuid}")
 //    public ResponseEntity<PopulationRatioResponse> getPopulationRatioByDistrictUuid(
