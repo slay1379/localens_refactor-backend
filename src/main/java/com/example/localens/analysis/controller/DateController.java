408a89e8c42bd688fdd69d0b423163a0f42524de
EgPwl <egpwl07@gmail.com>
2024-12-02 03:38:23 +0900

    [feat]: date compare 혼잡도 변화율

--- src/main/java/com/example/localens/analysis/controller/DateController.java
+++ src/main/java/com/example/localens/analysis/controller/DateController.java
@@ -1,5 +1,6 @@
 package com.example.localens.analysis.controller;
 
+import com.example.localens.analysis.service.DateCongestionRateService;
 import com.example.localens.analysis.service.DatePopulationService;
 import com.example.localens.analysis.service.DateStayVisitService;
 import com.example.localens.analysis.service.DateVisitConcentrationService;
@@ -18,6 +19,7 @@ public class DateController {
     private final DatePopulationService datePopulationService;
     private final DateVisitConcentrationService dateVisitConcentrationService;
     private final DateStayVisitService dateStayVisitService;
+    private final DateCongestionRateService dateCongestionRateService;
 
     @GetMapping("/population/{districtUuid}")
     public ResponseEntity<Map<String, Integer>> getPopulationResponse(
@@ -27,11 +29,13 @@ public class DateController {
         int normalizedPopulationValue = datePopulationService.getNormalizedPopulationValue(districtUuid, date);
         int visitConcentrationValue = dateVisitConcentrationService.getNormalizedPopulationValue(districtUuid, date);
         int stayVisitRatioValue = dateStayVisitService.getNormalizedStayVisitRatio(districtUuid, date);
+        int congestionRateValue = dateCongestionRateService.getNormalizedCongestionRate(districtUuid, date);
 
         Map<String, Integer> response = new LinkedHashMap<>();
         response.put("유동인구 수", normalizedPopulationValue);
         response.put("체류/방문 비율", stayVisitRatioValue);
         response.put("방문 집중도", visitConcentrationValue);
+        response.put("혼잡도_변화율", congestionRateValue);
 
         return ResponseEntity.ok(response);
     }
