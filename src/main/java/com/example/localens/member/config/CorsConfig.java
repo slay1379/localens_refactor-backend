ce0ebdf68cfe0b06107bf7ed314b036c32ea4932
slay <palgalow@gmail.com>
2024-12-01 20:21:57 +0900

    [feat]: 프론트엔드단 cors 설정 추가

--- src/main/java/com/example/localens/member/config/CorsConfig.java
+++ src/main/java/com/example/localens/member/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.localens.member.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
--- src/main/java/com/example/localens/member/config/MemberSecurityConfig.java
+++ src/main/java/com/example/localens/member/config/MemberSecurityConfig.java
@@ -33,17 +33,22 @@ public class MemberSecurityConfig {
         // CSRF 설정 Disable
         http.csrf(csrf -> csrf.disable());
 
+        // CORS 필터 추가
         http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
 
+        // Exception handling 설정
         http.exceptionHandling(exceptionHandling ->
                 exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint));
 
+        // H2 콘솔을 위한 설정
         http.headers(headers ->
                 headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
 
+        // 세션 관리 설정 -> STATELESS (JWT 사용 시 필요)
         http.sessionManagement(sessionManagement ->
-                sessionManagement.sessionCreationPolicy((SessionCreationPolicy.STATELESS)));
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        // 요청에 대한 권한 설정
         http.authorizeHttpRequests(authorize ->
                 authorize
                         .requestMatchers(
@@ -52,11 +57,15 @@ public class MemberSecurityConfig {
                                 "/swagger-ui/**",
                                 "/api-docs/**",
                                 "/swagger-resources/**",
-                                "/webjars/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
+                                "/webjars/**",
+                                "/api/member/signup",
+                                "/api/member/login",
+                                "/api/member/reissue"
+                        ).permitAll() // 인증 없이 접근을 허용하는 엔드포인트
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
         );
 
+        // JWT 관련 설정 추가
         http.apply(new JwtSecurityConfig(tokenProvider));
 
         return http.build();
