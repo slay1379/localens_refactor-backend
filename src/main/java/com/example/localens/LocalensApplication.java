e1daf762dc303bb5c952b716e52eb94edacc4b6c
EgPwl <egpwl07@gmail.com>
2024-12-01 19:39:39 +0900

    [refactor]: LocalensApplication에서 scanBasePakages 제거

--- src/main/java/com/example/localens/LocalensApplication.java
+++ src/main/java/com/example/localens/LocalensApplication.java
@@ -3,11 +3,7 @@ package com.example.localens;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.example.localens.analysis",
-        "com.example.localens.influx",
-        "com.example.localens.member"
-})
+@SpringBootApplication
 public class LocalensApplication {
 
     public static void main(String[] args) {
