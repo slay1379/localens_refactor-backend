e6004f04fed31535d73fac38bd00eab313d4457b
slay <palgalow@gmail.com>
2024-12-02 23:05:57 +0900

    [feat]: cors설정 추가

--- src/main/java/com/example/localens/member/config/MemberSecurityConfig.java
+++ src/main/java/com/example/localens/member/config/MemberSecurityConfig.java
@@ -58,6 +58,7 @@ public class MemberSecurityConfig {
                                 "/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**",
+                                "/api/**",
                                 "/api/member/**",
                                 "/api/datecompare/**",
                                 "/api/details/**",
