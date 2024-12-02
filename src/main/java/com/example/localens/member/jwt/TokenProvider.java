2b6071460558a5d88cbca6d9bbf989578a6c2c68
EgPwl <egpwl07@gmail.com>
2024-12-02 21:48:55 +0900

    [refactor]: accessToken expire time 12시간으로 변경

--- src/main/java/com/example/localens/member/jwt/TokenProvider.java
+++ src/main/java/com/example/localens/member/jwt/TokenProvider.java
@@ -22,7 +22,7 @@ import java.util.Date;
 @Component
 public class TokenProvider {
     private static final String BEARER_TYPE = "Bearer ";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 12;            // 12시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     private final Key key;
