bf9ddaa32346e73ba7b958d21a746a21d06eec5c
slay <palgalow@gmail.com>
2024-12-06 05:00:39 +0900

    [refactor]: 지표들 자료구조 수정

--- src/main/java/com/example/localens/improvement/controller/ImprovementController.java
+++ src/main/java/com/example/localens/improvement/controller/ImprovementController.java
@@ -161,19 +161,9 @@ public class ImprovementController {
                 radarInfoService
         );
 
-        logger.debug("district1Data: {}", district1Data);
-        logger.debug("district2Data: {}", district2Data);
-
         // 두 상권의 overallData 추출
-        Object district1OverallObj = district1Data.get("overallData");
-        Object district2OverallObj = district2Data.get("overallData");
-
-        // 안전하게 Map<String, Integer> 형태로 변환하는 헬퍼 메서드
-        Map<String, Integer> district1Overall = convertToIntegerMap(district1OverallObj);
-        Map<String, Integer> district2Overall = convertToIntegerMap(district2OverallObj);
-
-        logger.debug("district1Overall: {}", district1Overall);
-        logger.debug("district2Overall: {}", district2Overall);
+        Map<String, Integer> district1Overall = (Map<String, Integer>) district1Data.get("overallData");
+        Map<String, Integer> district2Overall = (Map<String, Integer>) district2Data.get("overallData");
 
         // 각 지표의 차이를 계산하여 저장할 리스트 생성
         List<Map.Entry<String, Integer>> differences = new ArrayList<>();
@@ -215,8 +205,8 @@ public class ImprovementController {
         List<Event> events = eventRepository.findAllById(eventUuids);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
-        DateTimeFormatter isoFormatter = DateTimeFormatter.ISO_DATE_TIME; // ISO 포맷 변환을 위한 포맷터
 
+        // 이벤트 정보를 improveMethod 리스트에 추가
         List<Map<String, Object>> improveMethodList = new ArrayList<>();
         List<Map<String, Object>> beforeOverallDataList = new ArrayList<>();
         List<Map<String, Object>> afterOverallDataList = new ArrayList<>();
@@ -230,70 +220,49 @@ public class ImprovementController {
                 Map<String, Object> improveMethod = new HashMap<>();
                 improveMethod.put("image", event.getEventImg());
                 improveMethod.put("name", event.getEventName());
-                improveMethod.put("date",
-                        event.getEventStart().format(formatter) + " ~ " + event.getEventEnd().format(formatter));
+                improveMethod.put("date", event.getEventStart().format(formatter) + " ~ " + event.getEventEnd().format(formatter));
                 improveMethod.put("area", event.getEventPlace());
                 improveMethod.put("detail", event.getInfo());
                 improveMethod.put("uuid", event.getEventUuid().toString());
                 improveMethodList.add(improveMethod);
 
-                String startDateTimeStr = event.getEventStart().format(isoFormatter);
-                String endDateTimeStr = event.getEventEnd().format(isoFormatter);
+                // beforeAndAfter 데이터 구성
+                LocalDate parsedDate1 = event.getEventStart().toLocalDate();
+                LocalDate parsedDate2 = event.getEventEnd().toLocalDate();
 
-                int normalizedPopulationValue1 = datePopulationService.getNormalizedPopulationValue(event.getEventPlaceInt(), startDateTimeStr);
-                int visitConcentrationValue1 = dateVisitConcentrationService.getNormalizedPopulationValue(event.getEventPlaceInt(), startDateTimeStr);
-                int stayVisitRatioValue1 = dateStayVisitService.getNormalizedStayVisitRatio(event.getEventPlaceInt(), startDateTimeStr);
-                int congestionRateValue1 = dateCongestionRateService.getNormalizedCongestionRate(event.getEventPlaceInt(), startDateTimeStr);
-                int stayPerVisitorValue1 = dateStayPerVisitorService.getNormalizedStayPerVisitorValue(event.getEventPlaceInt(), startDateTimeStr);
-                int stayDurationRateValue1 = dateStayDurationRateService.getNormalizedStayDurationRate(event.getEventPlaceInt(), startDateTimeStr);
+                Map<String, Object> beforeOverallData = new LinkedHashMap<>();
+                beforeOverallData.put("population", district1Overall.get("population"));
+                beforeOverallData.put("stayVisit", district1Overall.get("stayVisit"));
+                beforeOverallData.put("congestion", district1Overall.get("congestion"));
+                beforeOverallData.put("stayPerVisitor", district1Overall.get("stayPerVisitor"));
+                beforeOverallData.put("visitConcentration", district1Overall.get("visitConcentration"));
+                beforeOverallData.put("stayTimeChange", district1Overall.get("stayTimeChange"));
+                beforeOverallDataList.add(beforeOverallData);
+                beforeDates.add(parsedDate1.format(DateTimeFormatter.ofPattern("yyyy년 MM월")));
 
-                Map<String, Integer> values1 = new LinkedHashMap<>();
-                values1.put("population", normalizedPopulationValue1);
-                values1.put("stayVisit", stayVisitRatioValue1);
-                values1.put("visitConcentration", visitConcentrationValue1);
-                values1.put("congestion", congestionRateValue1);
-                values1.put("stayPerVisitor", stayPerVisitorValue1);
-                values1.put("stayTimeChange", stayDurationRateValue1);
+                Map<String, Object> afterOverallData = new LinkedHashMap<>();
+                afterOverallData.put("population", district2Overall.get("population"));
+                afterOverallData.put("stayVisit", district2Overall.get("stayVisit"));
+                afterOverallData.put("congestion", district2Overall.get("congestion"));
+                afterOverallData.put("stayPerVisitor", district2Overall.get("stayPerVisitor"));
+                afterOverallData.put("visitConcentration", district2Overall.get("visitConcentration"));
+                afterOverallData.put("stayTimeChange", district2Overall.get("stayTimeChange"));
+                afterOverallDataList.add(afterOverallData);
+                afterDates.add(parsedDate2.format(DateTimeFormatter.ofPattern("yyyy년 MM월")));
 
-                int normalizedPopulationValue2 = datePopulationService.getNormalizedPopulationValue(event.getEventPlaceInt(), endDateTimeStr);
-                int visitConcentrationValue2 = dateVisitConcentrationService.getNormalizedPopulationValue(event.getEventPlaceInt(), endDateTimeStr);
-                int stayVisitRatioValue2 = dateStayVisitService.getNormalizedStayVisitRatio(event.getEventPlaceInt(), endDateTimeStr);
-                int congestionRateValue2 = dateCongestionRateService.getNormalizedCongestionRate(event.getEventPlaceInt(), endDateTimeStr);
-                int stayPerVisitorValue2 = dateStayPerVisitorService.getNormalizedStayPerVisitorValue(event.getEventPlaceInt(), endDateTimeStr);
-                int stayDurationRateValue2 = dateStayDurationRateService.getNormalizedStayDurationRate(event.getEventPlaceInt(), endDateTimeStr);
-
-                Map<String, Integer> values2 = new LinkedHashMap<>();
-                values2.put("population", normalizedPopulationValue2);
-                values2.put("stayVisit", stayVisitRatioValue2);
-                values2.put("visitConcentration", visitConcentrationValue2);
-                values2.put("congestion", congestionRateValue2);
-                values2.put("stayPerVisitor", stayPerVisitorValue2);
-                values2.put("stayTimeChange", stayDurationRateValue2);
-
-                // 변화량 계산
-                Map<String, Integer> diffMap = new LinkedHashMap<>();
-                for (String key : values1.keySet()) {
-                    if (values2.containsKey(key)) {
-                        int v1 = values1.get(key);
-                        int v2 = values2.get(key);
-                        int diff = v2 - v1;
-                        diffMap.put(key, diff);
+                String biggestDifferenceMetric = null;
+                int biggestDifferenceValue = Integer.MIN_VALUE;
+                for (String key : district2Overall.keySet()) {
+                    if (district1Overall.containsKey(key)) {
+                        int difference = district2Overall.get(key) - district1Overall.get(key);
+                        if (difference > biggestDifferenceValue) {
+                            biggestDifferenceMetric = key;
+                            biggestDifferenceValue = difference;
+                        }
                     }
                 }
-
-                Entry<String, Integer> maxEntry = diffMap.entrySet().stream()
-                        .max(Entry.comparingByValue())
-                        .orElse(null);
-
-                if (maxEntry != null) {
-                    changedFeatureNames.add(maxEntry.getKey());
-                    changedFeatureValues.add(maxEntry.getValue());
-                }
-
-                beforeOverallDataList.add(Map.of("values", values1));
-                afterOverallDataList.add(Map.of("values", values2));
-                beforeDates.add(event.getEventStart().format(formatter));
-                afterDates.add(event.getEventEnd().format(formatter));
+                changedFeatureNames.add(biggestDifferenceMetric);
+                changedFeatureValues.add(biggestDifferenceValue);
             }
         }
 
@@ -308,40 +277,18 @@ public class ImprovementController {
         Map<String, Object> beforeAndAfter = new LinkedHashMap<>();
         beforeAndAfter.put("before", before);
         beforeAndAfter.put("after", after);
-        beforeAndAfter.put("changedFeature", Map.of(
-                "name", changedFeatureNames,
-                "value", changedFeatureValues
-        ));
+
+        Map<String, Object> changedFeature = new LinkedHashMap<>();
+        changedFeature.put("name", changedFeatureNames);
+        changedFeature.put("value", changedFeatureValues);
 
         // 최종 응답 구성
         Map<String, Object> response = new HashMap<>();
         response.put("ImproveMethod", improveMethodList);
         response.put("beforeAndAfter", beforeAndAfter);
+        response.put("changedFeature", changedFeature);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    /**
-     * overallData를 안전하게 Map<String, Integer> 형태로 변환하는 헬퍼 메서드.
-     * 해당 메서드는 전체 데이터 구조를 보장할 수 없으므로, 만약 값이 Integer가 아닌 경우 예외를 발생시킨다.
-     */
-    private Map<String, Integer> convertToIntegerMap(Object obj) {
-        if (!(obj instanceof Map)) {
-            throw new ClassCastException("The overallData is not of type Map");
-        }
-        Map<?, ?> rawMap = (Map<?, ?>) obj;
-        Map<String, Integer> intMap = new LinkedHashMap<>();
-        for (Map.Entry<?, ?> entry : rawMap.entrySet()) {
-            if (!(entry.getKey() instanceof String)) {
-                throw new ClassCastException("Key is not a String: " + entry.getKey());
-            }
-            if (!(entry.getValue() instanceof Integer)) {
-                throw new ClassCastException("Value is not an Integer for key: " + entry.getKey()
-                        + ", found: " + entry.getValue().getClass().getName());
-            }
-            intMap.put((String) entry.getKey(), (Integer) entry.getValue());
-        }
-        return intMap;
     }
 
 }
