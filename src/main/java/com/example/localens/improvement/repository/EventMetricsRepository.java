31b67b91c4bacbc6f090b78c61380e298170d4f1
slay <palgalow@gmail.com>
2024-12-05 20:58:34 +0900

    [refactor]: 레포지토리 수정

--- src/main/java/com/example/localens/improvement/repository/EventMetricsRepository.java
+++ src/main/java/com/example/localens/improvement/repository/EventMetricsRepository.java
@@ -5,9 +5,13 @@ import com.example.localens.improvement.domain.EventMetrics;
 import com.example.localens.improvement.domain.EventMetricsId;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventMetricsRepository extends JpaRepository<EventMetrics, EventMetricsId> {
     List<EventMetrics> findByMetricsUuidIn(List<String> metricsUuids);
 
-    List<String> findEventUuidByMetricsUuidIn(List<String> metricsUuids);
+    @Query("SELECT em.eventUuid FROM EventMetrics em WHERE em.metricsUuid IN :metricsUuids")
+    List<String> findEventUuidByMetricsUuidIn(@Param("metricsUuids") List<String> metricsUuids);
+
 }
--- src/main/java/com/example/localens/improvement/repository/MetricRepository.java
+++ src/main/java/com/example/localens/improvement/repository/MetricRepository.java
@@ -2,7 +2,11 @@ package com.example.localens.improvement.repository;
 
 import com.example.localens.improvement.domain.Metric;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MetricRepository extends JpaRepository<Metric, String> {
-    String findMetricsUuidByMetricsName(String metricsName);
+    @Query("SELECT m.metricsUuid FROM Metric m WHERE m.metricsName = :metricsName")
+    String findMetricsUuidByMetricsName(@Param("metricsName") String metricsName);
+
 }
