02cea90a3a9444284dc9a5ff3c1b585a67b914d7
slay <palgalow@gmail.com>
2025-01-05 20:16:11 +0900

    [feat]: DB에 있는 모든 상권 리스트 가져오는 기능 추가

--- src/main/java/com/example/localens/analysis/repository/CommercialDistrictRepository.java
+++ src/main/java/com/example/localens/analysis/repository/CommercialDistrictRepository.java
@@ -1,6 +1,7 @@
 package com.example.localens.analysis.repository;
 
 import com.example.localens.analysis.domain.CommercialDistrict;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface CommercialDistrictRepository extends JpaRepository<CommercialDistrict, Integer> {
     @Query("SELECT c.districtName FROM CommercialDistrict c WHERE c.districtUuid = :districtUuid")
     String findDistrictNameByDistrictUuid(@Param("districtUuid") Integer districtUuid);
+
+    @Query("SELECT DISTINCT c.districtName FROM CommercialDistrict c")
+    List<String> findAllPlaces();
 }
 
 
