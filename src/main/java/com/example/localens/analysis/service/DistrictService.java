4ac75dbeee5f75d8fc5377d7faeb695659333e6d
slay <palgalow@gmail.com>
2025-01-11 20:59:27 +0900

    [feat]: DistrictService 생성 - DistrictDTO 정보 조회하는 메소드 생성

---
+++ src/main/java/com/example/localens/analysis/service/DistrictService.java
@@ -0,0 +1,37 @@
+package com.example.localens.analysis.service;
+
+import com.example.localens.analysis.domain.Cluster;
+import com.example.localens.analysis.domain.CommercialDistrict;
+import com.example.localens.analysis.dto.ClusterDTO;
+import com.example.localens.analysis.dto.DistrictDTO;
+import com.example.localens.analysis.repository.ClusterRepository;
+import com.example.localens.analysis.repository.CommercialDistrictRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DistrictService {
+
+    private final CommercialDistrictRepository districtRepository;
+    private final ClusterRepository clusterRepository;
+
+    public DistrictDTO getDistrictDTO(Integer districtUuid) {
+        CommercialDistrict district = districtRepository.findById(districtUuid)
+                .orElseThrow(() -> new RuntimeException("District not found"));
+
+        DistrictDTO districtDTO = new DistrictDTO();
+        districtDTO.setDistrictUuid(district.getDistrictUuid());
+        districtDTO.setDistrictName(district.getDistrictName());
+
+        if (district.getCluster() != null) {
+            Cluster clusterEntity = district.getCluster();
+            ClusterDTO clusterDTO = new ClusterDTO();
+            clusterDTO.setClusterUuid(clusterEntity.getClusterUuid());
+            clusterDTO.setClusterName(clusterEntity.getClusterName());
+            districtDTO.setCluster(clusterDTO);
+        }
+
+        return districtDTO;
+    }
+}
