2336ba095e2a448084c1d9f180238f6fab937a2d
slay <palgalow@gmail.com>
2025-01-11 17:44:16 +0900

    [refactor]: RadarController 리팩토링 - 불필요한 코드 삭제

--- src/main/java/com/example/localens/analysis/controller/RadarController.java
+++ src/main/java/com/example/localens/analysis/controller/RadarController.java
@@ -1,9 +1,6 @@
 package com.example.localens.analysis.controller;
 
-import com.example.localens.analysis.domain.Pair;
-import com.example.localens.analysis.dto.*;
 import com.example.localens.analysis.service.*;
-import com.example.localens.analysis.util.RadarUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,57 +34,4 @@ public class RadarController {
 
         return ResponseEntity.ok(comparisonResult);
     }
-
-
-//    @GetMapping("/{districtUuid}")
-//    public ResponseEntity<RadarTopTwoResponse> getOverallData(@PathVariable Integer districtUuid) {
-//        // 각 서비스 호출 결과를 변수에 저장
-//        RadarFloatingPopulationResponse floatingPopulation = radarFloatingPopulationService.getNormalizedFloatingPopulation(districtUuid);
-//        RadarStayVisitRatioResponse stayVisitRatio = radarStayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid);
-//        RadarCongestionRateResponse congestionRate = radarCongestionRateService.getCongestionRateByDistrictUuid(districtUuid);
-//        RadarStayPerVisitorResponse stayPerVisitor = radarStayPerVisitorService.getStayPerVisitorByDistrictUuid(districtUuid);
-//        RadarStayDurationChangeResponse stayDurationChange = radarStayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid);
-//
-//        // 결과 리스트에 추가
-//        List<Object> overallData = new ArrayList<>();
-//        overallData.add(floatingPopulation);
-//        overallData.add(stayVisitRatio);
-//        overallData.add(congestionRate);
-//        overallData.add(stayPerVisitor);
-//        overallData.add(stayDurationChange);
-//
-//        // 상위 두 항목 찾기
-//        String[] topTwo = RadarUtils.findTopTwo(overallData);
-//
-//        // 결과를 DTO로 반환
-//        RadarTopTwoResponse response = new RadarTopTwoResponse(overallData, topTwo);
-//
-//        return ResponseEntity.ok(response);
-//    }
-
-
-//    @GetMapping("/floating-population/{districtUuid}")
-//    public RadarFloatingPopulationResponse getFloatingPopulation(@PathVariable Integer districtUuid) {
-//        return radarFloatingPopulationService.getNormalizedFloatingPopulation(districtUuid);
-//    }
-//
-//    @GetMapping("/stay-visit-ratio/{districtUuid}")
-//    public RadarStayVisitRatioResponse getStayVisitRatio(@PathVariable Integer districtUuid) {
-//        return radarStayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid);
-//    }
-//
-//    @GetMapping("/congestion-rate/{districtUuid}")
-//    public RadarCongestionRateResponse getCongestionRate(@PathVariable Integer districtUuid) {
-//        return radarCongestionRateService.getCongestionRateByDistrictUuid(districtUuid);
-//    }
-//
-//    @GetMapping("/stay-per-visitor/{districtUuid}")
-//    public RadarStayPerVisitorResponse getStayPerVisitor(@PathVariable Integer districtUuid) {
-//        return radarStayPerVisitorService.getStayPerVisitorByDistrictUuid(districtUuid);
-//    }
-//
-//    @GetMapping("/stay-duration-change/{districtUuid}")
-//    public RadarStayDurationChangeResponse getAvgStayDurationChange(@PathVariable Integer districtUuid) {
-//        return radarStayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid);
-//    }
 }
