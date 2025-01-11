f6893d5f2b34a82ec11cd6bc72747d86684d2aac
slay <palgalow@gmail.com>
2025-01-11 17:43:26 +0900

    [refactor]: PopulationController 리팩토링 - 불필요한 코드 삭제

--- src/main/java/com/example/localens/analysis/controller/PopulationController.java
+++ src/main/java/com/example/localens/analysis/controller/PopulationController.java
@@ -1,6 +1,5 @@
 package com.example.localens.analysis.controller;
 
-import com.example.localens.analysis.dto.*;
 import com.example.localens.analysis.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,69 +23,4 @@ public class PopulationController {
 
         return ResponseEntity.ok(results);
     }
-
-//    @GetMapping("/all-ratios/{districtUuid}")
-//    public ResponseEntity<List<Object>> getAllRatiosByDistrictUuid(@PathVariable Integer districtUuid) {
-//        List<Object> results = new ArrayList<>();
-//
-//        // 각 서비스 호출 결과를 리스트에 추가
-//        results.add(populationRatioService.getPopulationRatioByDistrictUuid(districtUuid));
-//        results.add(stayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid));
-//        results.add(congestionRateService.getCongestionRateByDistrictUuid(districtUuid));
-//        results.add(stayPerVisitorService.getStayPopulationRatioByDistrictUuid(districtUuid));
-//        results.add(stayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid));
-//        results.add(ageGenderRatioService.getAgeGenderPopulationRatio(districtUuid));
-//        results.add(nationalityRatioService.getNationalityPopulationRatio(districtUuid));
-//
-//        return ResponseEntity.ok(results);
-//    }
-
-//    @GetMapping("/population-ratio/{districtUuid}")
-//    public ResponseEntity<PopulationRatioResponse> getPopulationRatioByDistrictUuid(
-//            @PathVariable Integer districtUuid) {
-//        PopulationRatioResponse result = populationRatioService.getPopulationRatioByDistrictUuid(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    @GetMapping("/stay-visit-ratio/{districtUuid}")
-//    public ResponseEntity<StayVisitRatioResponse> getStayVisitRatioByDistrictUuid(
-//            @PathVariable Integer districtUuid) {
-//        StayVisitRatioResponse result = stayVisitRatioService.getStayVisitRatioByDistrictUuid(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    @GetMapping("/congestion-rate/{districtUuid}")
-//    public ResponseEntity<CongestionRateResponse> getCongestionRateByDistrictUuid(
-//            @PathVariable Integer districtUuid) {
-//        CongestionRateResponse result = congestionRateService.getCongestionRateByDistrictUuid(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    @GetMapping("/stay-population-ratio/{districtUuid}")
-//    public ResponseEntity<StayPerVisitorResponse> getStayPopulationRatioByDistrictUuid(
-//            @PathVariable Integer districtUuid) {
-//        StayPerVisitorResponse result = stayPerVisitorService.getStayPopulationRatioByDistrictUuid(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    @GetMapping("/stay-duration-rate/{districtUuid}")
-//    public ResponseEntity<StayDurationChangeResponse> getAvgStayTimeChangeRate(
-//            @PathVariable Integer districtUuid) {
-//        StayDurationChangeResponse result = stayDurationChangeService.calculateAvgStayTimeChangeRate(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    @GetMapping("/age-gender-ratio/{districtUuid}")
-//    public ResponseEntity<AgeGenderRatioResponse> getAgeGenderPopulationRatio(
-//            @PathVariable Integer districtUuid) {
-//        AgeGenderRatioResponse result = ageGenderRatioService.getAgeGenderPopulationRatio(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
-//
-//    @GetMapping("/nationality-ratio/{districtUuid}")
-//    public ResponseEntity<NationalityRatioResponse> getNationalityRatioByDistrictUuid(
-//            @PathVariable Integer districtUuid) {
-//        NationalityRatioResponse result = nationalityRatioService.getNationalityPopulationRatio(districtUuid);
-//        return ResponseEntity.ok(result);
-//    }
 }
