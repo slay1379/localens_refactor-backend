69209ecbb3ffced4287a2bd29077a29975b1bb70
slay <palgalow@gmail.com>
2025-01-11 20:58:28 +0900

    [feat]: 두 상권의 차이나는 두 지표값을 를 담을 DTO 생성

---
+++ src/main/java/com/example/localens/analysis/dto/DifferenceItemDTO.java
@@ -0,0 +1,17 @@
+package com.example.localens.analysis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DifferenceItemDTO {
+    private String name;
+    private int value1;
+    private int value2;
+    private double difference;
+}
