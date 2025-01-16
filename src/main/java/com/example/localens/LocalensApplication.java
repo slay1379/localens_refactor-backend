01a0e5f2d0c395be148ce5f98451961042c22bc2
slay <palgalow@gmail.com>
2025-01-16 16:48:30 +0900

    [refactor]: Application 리팩토링 - min/max 값 바로 테스트 할 수 있도록 수정

--- src/main/java/com/example/localens/LocalensApplication.java
+++ src/main/java/com/example/localens/LocalensApplication.java
@@ -1,13 +1,25 @@
 package com.example.localens;
 
+import com.example.localens.analysis.service.StatsBatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class LocalensApplication {
+@RequiredArgsConstructor
+public class LocalensApplication implements CommandLineRunner {
 
+    private final StatsBatchService statsBatchService;
     public static void main(String[] args) {
         SpringApplication.run(LocalensApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        statsBatchService.updateMinMaxStatistics();
     }
 
 }
