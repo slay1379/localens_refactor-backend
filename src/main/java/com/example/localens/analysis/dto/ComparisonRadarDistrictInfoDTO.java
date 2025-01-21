33b719bc259ce387464314be3d2d0b66eda80d26
slay <palgalow@gmail.com>
2025-01-21 14:49:20 +0900

    [refactor]: ComparisonRadarDistrictInfoDTO - 생성자 어노테이션 제거

--- src/main/java/com/example/localens/analysis/dto/ComparisonRadarDistrictInfoDTO.java
+++ src/main/java/com/example/localens/analysis/dto/ComparisonRadarDistrictInfoDTO.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ComparisonRadarDistrictInfoDTO extends BaseRadarDistrictInfoDTO {
-    // 추가 필드가 필요하다면 여기에 추가
+
 }
