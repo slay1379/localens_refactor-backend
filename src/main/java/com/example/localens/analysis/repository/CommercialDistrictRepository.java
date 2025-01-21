cfee05465878eb956afa5ed05fff877b389e6e66
slay <palgalow@gmail.com>
2025-01-21 13:52:00 +0900

    [feat]: CommercialDistrictRepository - N+1 문제 해결을 위해 전체 엔티티를 한 번에 조회할 수 있도록 함

--- src/main/java/com/example/localens/analysis/repository/CommercialDistrictRepository.java
+++ src/main/java/com/example/localens/analysis/repository/CommercialDistrictRepository.java
@@ -2,6 +2,7 @@ package com.example.localens.analysis.repository;
 
 import com.example.localens.analysis.domain.CommercialDistrict;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +15,11 @@ public interface CommercialDistrictRepository extends JpaRepository<CommercialDi
 
     @Query("SELECT DISTINCT c.districtName FROM CommercialDistrict c")
     List<String> findAllPlaces();
+
+    Optional<CommercialDistrict> findByDistrictUuid(Integer districtUuid);
+
+    @Query("SELECT c FROM CommercialDistrict c WHERE c.districtUuid = :districtUuid")
+    Optional<CommercialDistrict> findCompleteDistrictByUuid(@Param("districtUuid") Integer districtUuid);
 }
 
 
